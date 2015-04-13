4ed3c32da7dbba5bbff94e752e0c0822c733ef2d
shaanamin13 <shaan-amin@uiowa.edu>
2015-04-13 13:55:13 -0500

    Hi

--- mysite/code/ExhibitionHolder.php
+++ mysite/code/ExhibitionHolder.php
@@ -23,10 +23,39 @@ class ExhibitionHolder extends Page {
 }
 
 class ExhibitionHolder_Controller extends Page_Controller {
+	private static $allowed_actions = array('exhibition');
+
+	private static $url_handlers = array(
+        'exhibition/$StartDate/$EndDate' => 'payroll',
+        
+    );
+    $exhibitions = ExhibitionPage::get();
 
 	public function init() {
 		parent::init();
 
+	}
+
+	public function upcoming(){
+		foreach ($exhibitions as $exhibition) {
+				if ($exhibition->StartDate > TODAY) {
+					$upcomingExhibitions = $upcomingExhibitions->push($exhibition);
+				}
+			}
+			return $upcomingExhibitions;
+		}
+	}
+	public function past(){
+		foreach ($exhibitions as $exhibition) {
+				if ($exhibition->EndDate < TODAY) {
+					$pastExhibitions = $pastExhibitions->push($exhibition);
+				}
+			}
+			return $pastExhibitions;
+		}
+	}
+	public function current(){
+		
 	}
 
 }
