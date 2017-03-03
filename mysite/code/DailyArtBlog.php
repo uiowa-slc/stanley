3508fd33e490c93b630f3179bcbf2362e4fec9d9
Dustin Quam <dustinquam@me.com>
2017-03-03 14:42:58 -0600

    singular and plural names

--- mysite/code/DailyArtBlog.php
+++ mysite/code/DailyArtBlog.php
@@ -9,6 +9,9 @@ class DailyArtBlog extends Blog {
 	private static $allowed_children = array(
 		'DailyArtBlogPost'
 	);
+    private static $singular_name = 'Daily Art Blog';
+
+    private static $plural_name = 'Daily Art Blogs';
 
 	public function getCMSFields() {
 		$fields = parent::getCMSFields();
@@ -56,25 +59,5 @@ class DailyArtBlog_Controller extends Blog_Controller {
         return $this->renderWith('DailyArtBlogDaySlider');
     }
 
-
-    public function NextPage(){
-    	$currentDate = SS_Datetime::now();
-    	$nextDay = $currentDate->next_day($currentDate->Format('Y'),$currentDate->Format('n'),$currentDate->Format('j'));
-
-    	$nextDayObj = DailyArtBlogDay::create();
-    	$nextDayObj->Date = $nextDay;
-    	// print_r($nextDayObj->obj('Date'));
-    	return $nextDayObj->getLatestDailyArtBlogPost();
-    }
-
-    public function PreviousPage(){
-    	$currentDate = SS_Datetime::now();
-    	$nextDay = $currentDate->day_before($currentDate->Format('Y'),$currentDate->Format('n'),$currentDate->Format('j'));
-
-    	$nextDayObj = DailyArtBlogDay::create();
-    	$nextDayObj->Date = $nextDay;
-    	// print_r($nextDayObj->obj('Date'));
-    	return $nextDayObj->getLatestDailyArtBlogPost();
-    }
 
 }
--- mysite/code/DailyArtBlogPost.php
+++ mysite/code/DailyArtBlogPost.php
@@ -20,6 +20,10 @@ class DailyArtBlogPost extends BlogPost {
 
     );
 
+    private static $singular_name = 'Daily Art Post';
+
+    private static $plural_name = 'Daily Art Posts';
+
     protected function onBeforeWrite(){
     	$blogDay = DataObject::get_one('DailyArtBlogDay', array('Month' => $this->obj('PublishDate')->format('m'), 'Date' => $this->obj('PublishDate')->format('d')));
     	if($blogDay){
@@ -57,77 +61,6 @@ class DailyArtBlogPost extends BlogPost {
 	    return $posts;
 	}
 
-	public function NextPage(){
-
-	}
-
-	// public function PreviousPage(){
-	// 	// $prevPost = DailyArtBlogPost::get()->filter(array(
-	// 	// 	'PublishDate:LessThan' => $this->PublishDate
-	// 	// ))->First();
-
-	// 	// return $prevPost;
-	// 	//Sorta working query:
-	// 	//SELECT DailyArtBlogPost.ID, BlogPost.ID, BlogPost.PublishDate FROM DailyArtBlogPost LEFT JOIN BlogPost ON DailyArtBlogPost.ID=BlogPost.ID
-	// 		//NOT WORKIBNG:
-	// 		//SELECT DailyArtBlogPost.ID, BlogPost.PublishDate, BlogPost.ID FROM DailyArtBlogPost LEFT JOIN "BlogPost" ON DailyArtBlogPost.ID=BlogPost.ID
-	// 	$sqlQuery = new SQLSelect();
-
-	// 	$sqlQuery->setSelect('DailyArtBlogPost.ID');
-	// 	$sqlQuery->selectField('BlogPost.ID');
-	// 	$sqlQuery->selectField('BlogPost.PublishDate');
-
-
-	// 	$sqlQuery->setFrom('DailyArtBlogPost');
-	// 	$sqlQuery->addLeftJoin('BlogPost','"DailyArtBlogPost"."ID"="BlogPost"."ID"');
-	// 	//$sqlQuery->setWhere('DailyArtBlogPost.ID = '.$this->ID);
-	// 	//$sqlQuery->addOrderBy("CASE WHEN BlogPost.ID >= ".$this->ID." THEN 0 ELSE 1 END ASC, BlogPost.ID ASC");
-	// 	//$sqlQuery->setOrderBy("STR_TO_DATE( BlogPost.PublishDate, '%m/%d/%Y' ) ASC");
-	// 	//$sqlQuery->setOrderBy("BlogPost.PublishDate DESC");
-	// 	//$sqlQuery->setOrderBy("STR_TO_DATE( BlogPost.PublishDate, '%m/%d/%Y' ) ASC");
-
-	// 	// $sqlQuery->setOrderBy("MONTH(BlogPost.PublishDate) DESC, DAY(BlogPost.PublishDate) DESC, CASE WHEN BlogPost.ID >= ".$this->ID." THEN 0 ELSE 1 END ASC");
-
-	// 	$sqlQuery->addOrderBy("MONTH(BlogPost.PublishDate) DESC, DAY(BlogPost.PublishDate) DESC");
-	// 	// $sqlQuery->addOrderBy("");
-	// 	//$sqlQuery->setLimit(1,1);
-	// 	// print_r($sqlQuery->__toString());
-	// 	$result = $sqlQuery->execute();
-	// 	// Iterate over results
-	// 	foreach($result as $row) {
-	// 	  echo $row['ID'].': '.$row['PublishDate'].'<br />';
-	// 	}
-	// }
-
-	public function PreviousPage(){
-		// $prevPost = DailyArtBlogPost::get()->sort(array('STR_TO_DATE( "PublishDate", "%m/%d/%Y" )'=>'DESC'));
-		$posts = DailyArtBlogPost::get()->sort('MONTH(PublishDate) DESC, DAY(PublishDate) DESC');
-		// $thisPost = $posts->filter(array('ID' =>$this->ID));
-		//  //print_r($prevPost->sql());
-		// print_r($thisPost->toArray());
-		$postsCount = $posts->Count() - 1;
-		foreach($posts as $key => $post) {
-			if($post->ID == $this->ID){
-				// print_r($posts->Count())
-				if($key == $postsCount){
-					$nextKey = 0;
-				}else{
-					$nextKey = $key + 1;
-				}
-
-				if($key == 0){
-					$prevKey = $postsCount;
-				}else{
-					$prevKey = $key - 1;
-				}
-
-				//echo '<strong>key:'.$key.', post: '.$post->ID.': '.$post->PublishDate.', next key: '.$nextKey.', prev key: '.$prevKey.'</strong><br />';
-			}else{
-				//echo 'key:'.$key.', post: '.$post->ID.': '.$post->PublishDate.'<br />';
-			}
-
-		}
-	}
 
 }
 
