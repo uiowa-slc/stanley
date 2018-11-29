79c7756e3e88fc2542d67212eb7c3e1c06b99526
Dustin Quam <quamsta@gmail.com>
2018-11-29 14:00:05 -0600

    added test staff holder page

---
+++ mysite/code/StaffHolderPage.php
@@ -0,0 +1,23 @@
+<?php
+
+class StaffHolderPage extends Page {
+	private static $db = array(
+
+	);
+
+	private static $has_one = array (
+	
+	);
+	private static $defaults = array (
+		
+	);
+
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+
+		return $fields;
+	}
+
+
+}
+
