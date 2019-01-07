d5650c061cf559bbab202437c6af26abaf06327c
Dustin Quam <quamsta@gmail.com>
2019-01-07 11:20:08 -0600

    change limit children

--- mysite/code/StaffHolderPage.php
+++ mysite/code/StaffHolderPage.php
@@ -2,41 +2,41 @@
 class StaffHolderPage extends Page {
 
 	private static $db = array(
-		
+
 	);
 
 	private static $has_one = array(
-	
+
 	);
 
 	// private static $belongs_many_many = array(
 	// 	"Teams" => "StaffTeam"
 	// );
 
-	private static $allowed_children = array("StaffPage", "VirtualPage", "UserDefinedForm");
-	
+	private static $allowed_children = array("StaffPage","*Page", "UserDefinedForm");
+
 	public function getCMSFields(){
 		$f = parent::getCMSFields();
-		
+
 		// $f->addFieldToTab('Root.Main', new CheckboxSetField("Teams", 'Show the following staff teams on this page:', StaffTeam::get()->map('ID', 'Title')), 'Content');
-		
+
 		//$f->removeByName("Content");
 		$gridFieldConfig = GridFieldConfig_RecordEditor::create();
 		$gridFieldConfig->addComponent(new GridFieldSortableRows('SortOrder'));
-		
-		
+
+
 		$gridField = new GridField("StaffTeam", "Staff Teams", StaffTeam::get(), $gridFieldConfig);
 		$f->addFieldToTab("Root.Main", $gridField, "Content"); // add the grid field to a tab in the CMS
 		      $this->extend('updateCMSFields', $f);
 		return $f;
 	}
-	
+
 	public function Children(){
 		$staffPages = parent::Children()->sort('LastName');
 		$this->extend('alterChildren', $staffPages);
 		return $staffPages;
 	}
-	
+
 	public function StaffTeams(){
 		$teams = StaffTeam::get();
 		return $teams;
