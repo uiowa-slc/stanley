62a836e3a5e4d87e08c764c47d01a18bf01fa61c
Dustin Quam <quamsta@gmail.com>
2020-01-08 09:35:28 -0600

    force https

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,6 +3,7 @@
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
 use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
+use SilverStripe\Control\Director;
 // remove PasswordValidator for SilverStripe 5.0
 $validator = new PasswordValidator();
 
@@ -11,6 +12,11 @@ $validator->checkHistoricalPasswords(6);
 Member::set_password_validator($validator);
 SilverStripe\ORM\Search\FulltextSearchable::enable();
 
+if(Director::isLive()) {
+	Director::forceSSL();
+}
+
 TinyMCEConfig::get('cms')
     ->addButtonsToLine(1, 'styleselect')
     ->setOption('importcss_append', true);
+
